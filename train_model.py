3a4683e5b1db768f68d2678694155cf1e37750bb
ashiba713 <ashibaalben7744@gmail.com>
2025-12-27 20:41:15 +0530

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+df = pd.read_csv("data/students.csv")
+
+X = df.drop("dropout", axis=1)
+y = df["dropout"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+preds = model.predict(X_test)
+
+print(classification_report(y_test, preds))
